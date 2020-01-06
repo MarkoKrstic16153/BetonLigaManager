dd44e81c487012af7a5aa6fce04ffe737a983b10
vladeta97 <vladeta.manic@elfak.rs>
2020-01-06 16:38:46 +0100

    slicice

--- praviUI/src/app/components/igrac/igrac.component.ts
+++ praviUI/src/app/components/igrac/igrac.component.ts
@@ -1,53 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { Igrac } from 'src/models/Igrac';
-import { IgracService } from 'src/services/IgracService';
+import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
+import { Igrac } from "src/models/Igrac";
+import { IgracService } from "src/services/IgracService";
 
 @Component({
-  selector: 'app-igrac',
-  templateUrl: './igrac.component.html',
-  styleUrls: ['./igrac.component.css']
+  selector: "app-igrac",
+  templateUrl: "./igrac.component.html",
+  styleUrls: ["./igrac.component.css"]
 })
 export class IgracComponent implements OnInit {
-
   igrac;
-  loadingIgrac:boolean=true;
-  loadingByPostion:boolean=true;
-  loadingByNumber:boolean=true;
-  loadingSaigraci:boolean=true;
-  poPoziciji:Igrac[];
-  poBrojDresa:Igrac[];
-  saigraci:Igrac[];
-  constructor(private location:Location,private active:ActivatedRoute,private igracService:IgracService) { }
+  loadingIgrac: boolean = true;
+  loadingByPostion: boolean = true;
+  loadingByNumber: boolean = true;
+  loadingSaigraci: boolean = true;
+  poPoziciji: Igrac[];
+  poBrojDresa: Igrac[];
+  saigraci: Igrac[];
+  constructor(
+    private location: Location,
+    private active: ActivatedRoute,
+    private igracService: IgracService
+  ) {}
 
   ngOnInit() {
-    this.active.params.subscribe(params=>{
-      this.igracService.getIgrac(params.id).subscribe(({data,loading})=>{
-        this.loadingIgrac=loading;
-        this.igrac=data.Igrac[0];
+    this.active.params.subscribe(params => {
+      this.igracService.getIgrac(params.id).subscribe(({ data, loading }) => {
+        this.loadingIgrac = loading;
+        this.igrac = data.Igrac[0];
         //povlaci sve saigrace iz tima
-        this.igracService.getIgraciTim(this.igrac.tim[0].Tim.naziv).subscribe(({data,loading})=>{
-          this.loadingSaigraci=loading;
-          this.saigraci=data.Tim[0].igraci;
-        });
+        this.igracService
+          .getIgraciTim(this.igrac.tim[0].Tim.naziv)
+          .subscribe(({ data, loading }) => {
+            this.loadingSaigraci = loading;
+            this.saigraci = data.Tim[0].igraci;
+          });
 
         //povlaci 5 igraca sa istom pozicijom bez igraca koji se prikazuje trenutno
-        this.igracService.getIgracByPosition(this.igrac.pozicija,this.igrac.brojTelefona).subscribe(({data,loading})=>{
-          this.loadingByPostion=loading;
-          this.poPoziciji=data.Igrac.slice(0,5);
-        });
+        this.igracService
+          .getIgracByPosition(this.igrac.pozicija, this.igrac.brojTelefona)
+          .subscribe(({ data, loading }) => {
+            this.loadingByPostion = loading;
+            this.poPoziciji = data.Igrac.slice(0, 5);
+          });
         //povlaci 5 igraca sa istim brojem bez igraca koji se prikazuje trenutno
-        this.igracService.getIgracByNumber(this.igrac.brojDresa,this.igrac.brojTelefona).subscribe(({data,loading})=>{
-          this.loadingByNumber=loading;
-          this.poBrojDresa=data.Igrac.slice(0,5);
-        });
-;        
-      })
-    })
+        this.igracService
+          .getIgracByNumber(this.igrac.brojDresa, this.igrac.brojTelefona)
+          .subscribe(({ data, loading }) => {
+            this.loadingByNumber = loading;
+            this.poBrojDresa = data.Igrac.slice(0, 5);
+          });
+      });
+    });
   }
-  
-  goBack(){
+
+  goBack() {
     this.location.back();
   }
 }
--- praviUI/src/services/IgracService.ts
+++ praviUI/src/services/IgracService.ts
@@ -1,294 +1,326 @@
-import { Injectable } from '@angular/core';
-import { QueryRef, Apollo } from 'apollo-angular';
-import { Igrac } from '../models/Igrac';
-import gql from 'graphql-tag';
-import { variable } from '@angular/compiler/src/output/output_ast';
+import { Injectable } from "@angular/core";
+import { QueryRef, Apollo } from "apollo-angular";
+import { Igrac } from "../models/Igrac";
+import gql from "graphql-tag";
+import { variable } from "@angular/compiler/src/output/output_ast";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class IgracService {
-    query: QueryRef<any>;
-    constructor(private apollo: Apollo) { 
-
-    }
-    addIgrac(noviIgrac:Igrac,tim:string){
-        let ADD_IGRAC = gql`
-        mutation dodajIgraca(
-            $ime:String!,
-            $prezime:String!,
-            $brojDresa:String!,
-            $godRodjenja:String!,
-            $brojTelefona:String!,
-            $pozicija:String!,
-            $opis:String!
-        ){ 
-            CreateIgrac(
-                ime: $ime,
-                prezime: $prezime,
-                brojDresa: $brojDresa,
-                godRodjenja: $godRodjenja,
-                brojTelefona: $brojTelefona,
-                pozicija: $pozicija,
-                opis: $opis
-            ){
-                ime
-            }
-        }`;
-        return this.apollo.mutate({
-            mutation:ADD_IGRAC,
-            variables:{
-                ime:noviIgrac.ime,
-                prezime:noviIgrac.prezime,
-                brojDresa:noviIgrac.brojDresa,
-                godRodjenja:noviIgrac.godRodjenja,
-                brojTelefona:noviIgrac.brojTelefona,
-                pozicija:noviIgrac.pozicija,
-                opis:noviIgrac.opis,
-            }
-        });
-    }
-    getIgraciTim(nazivTima:string){
-        let GET_IGRACI_TIM = gql`
-        query GET_IGRACI_TIM($nazivTima:String!)
-        {
-            Tim(filter:{naziv:$nazivTima}){
-                igraci{
-                    Igrac
-                    {
-                        ime,
-                        prezime,
-                        brojDresa,
-                        godRodjenja,
-                        brojTelefona,
-                        pozicija,
-                        opis
-                    }
-                }
-            }
-        }`;
-        this.query = this.apollo.watchQuery({
-            query: GET_IGRACI_TIM,
-            variables: {nazivTima:nazivTima}
-          });
-        return this.query.valueChanges;
-    }
-    addIgracToTim(brojTelefonaIgraca:String,nazivTima:String){
-        let ADD_IGRAC_TO_TIM=gql`
-        mutation ADD_IGRAC_U_TIM($brojTelefonaIgraca:String!,$nazivTima:String!){
-            AddIgracTim(from:{brojTelefona:$brojTelefonaIgraca},to:{naziv:$nazivTima})
-            {
-                from{
-                    ime,
-                    prezime,
-                    brojTelefona
-                }
-                to{
-                    naziv
-                }
-            }
-            }`;
-        return this.apollo.mutate({
-            mutation:ADD_IGRAC_TO_TIM,
-            variables:{
-                brojTelefonaIgraca:brojTelefonaIgraca,
-                nazivTima:nazivTima
-            }
-        })
-    }
-    addIgracGol(brojTelefonaIgraca:String,datumUtakmice:String,vremeUtakmice:String,vremeGola:String){
-        let ADD_IGRAC_GOL_UTAKMICA=gql`
-        mutation ADD_IGRAC_GOL_UTAKMICA($brojTelefonaIgraca:String!,$datumUtakmice:String!,$vremeUtakmice:String!,$vremeGola:String!){
-            AddIgracGolovi(
-                from:{brojTelefona:$brojTelefonaIgraca},
-                to:{datum:$datumUtakmice,vreme:$vremeUtakmice},
-                data:{vreme:$vremeGola})
-            {
-                from{
-                    ime,
-                    prezime,
-                    brojTelefona
-                }
-                to{
-                    naziv,
-                    datum,
-                    vreme
-                }
-            }
-        }`;
-        return this.apollo.mutate({
-            mutation:ADD_IGRAC_GOL_UTAKMICA,
-            variables:{
-                brojTelefonaIgraca:brojTelefonaIgraca,
-                datumUtakmice:datumUtakmice,
-                vremeUtakmice:vremeUtakmice,
-                vremeGola:vremeGola
-            }
-        })
-    }
-    addIgracToUtakmica(brojTelefonaIgraca:String,datumUtakmice:String,vremeUtakmice:String){
-        let ADD_IGRAC_U_UTAKMICU=gql`
-        mutation ADD_IGRAC_U_UTAKMICU($brojTelefonaIgraca:String!,$datumUtakmice:String!,$vremeUtakmice:String!){
-        AddIgracUtakmice(from:{brojTelefona:$brojTelefonaIgraca},to:{datum:$datumUtakmice,vreme:$vremeUtakmice})
-        {
-            from{
-                ime,
-                prezime,
-                brojTelefona
-            }
-            to{
-                naziv,
-                datum,
-                vreme
-            }
+  query: QueryRef<any>;
+  constructor(private apollo: Apollo) {}
+  addIgrac(noviIgrac: Igrac, tim: string) {
+    let ADD_IGRAC = gql`
+      mutation dodajIgraca(
+        $ime: String!
+        $prezime: String!
+        $brojDresa: String!
+        $godRodjenja: String!
+        $brojTelefona: String!
+        $pozicija: String!
+        $opis: String!
+      ) {
+        CreateIgrac(
+          ime: $ime
+          prezime: $prezime
+          brojDresa: $brojDresa
+          godRodjenja: $godRodjenja
+          brojTelefona: $brojTelefona
+          pozicija: $pozicija
+          opis: $opis
+        ) {
+          ime
         }
-        }`;
-        return this.apollo.mutate({
-            mutation:ADD_IGRAC_U_UTAKMICU,
-            variables:{
-                brojTelefonaIgraca:brojTelefonaIgraca,
-                datumUtakmice:datumUtakmice,
-                vremeUtakmice:vremeUtakmice
+      }
+    `;
+    return this.apollo.mutate({
+      mutation: ADD_IGRAC,
+      variables: {
+        ime: noviIgrac.ime,
+        prezime: noviIgrac.prezime,
+        brojDresa: noviIgrac.brojDresa,
+        godRodjenja: noviIgrac.godRodjenja,
+        brojTelefona: noviIgrac.brojTelefona,
+        pozicija: noviIgrac.pozicija,
+        opis: noviIgrac.opis
+      }
+    });
+  }
+  getIgraciTim(nazivTima: string) {
+    let GET_IGRACI_TIM = gql`
+      query GET_IGRACI_TIM($nazivTima: String!) {
+        Tim(filter: { naziv: $nazivTima }) {
+          igraci {
+            Igrac {
+              ime
+              prezime
+              brojDresa
+              godRodjenja
+              brojTelefona
+              pozicija
+              opis
             }
-        })
-    }
-    getIgracStartsWith(pocetakImena:String)
-    {
-        let GET_IGRAC_STARTS_WITH=gql`
-        query GET_IGRAC_STARTS_WITH($pocetakImena:String!){
-        Igrac(filter:{ime_starts_with:$pocetakImena})
-        {
-            ime,
-            prezime,
-            brojDresa,
-            brojTelefona,
-            tim{
-                Tim{
-                    naziv
-                }
-            }
+          }
         }
-        }`;
-        this.query= this.apollo.watchQuery({
-            query:GET_IGRAC_STARTS_WITH,
-            variables:{
-                pocetakImena:pocetakImena
-            }
-        });
-        return this.query.valueChanges;
-    }
-    getIgracByPosition(pozicija:String,brojTelefona?:String)
-    {
-        let GET_IGRAC_BY_POSITION=gql`
-        query GET_IGRAC_BY_POSITION($pozicija:String!,$brojTelefonaIgraca:String){
-        Igrac(filter:{pozicija:$pozicija,brojTelefona_not:$brojTelefonaIgraca})
-        {
-            ime,
-            prezime,
-            brojDresa,
+      }
+    `;
+    this.query = this.apollo.watchQuery({
+      query: GET_IGRACI_TIM,
+      variables: { nazivTima: nazivTima }
+    });
+    return this.query.valueChanges;
+  }
+  addIgracToTim(brojTelefonaIgraca: String, nazivTima: String) {
+    let ADD_IGRAC_TO_TIM = gql`
+      mutation ADD_IGRAC_U_TIM(
+        $brojTelefonaIgraca: String!
+        $nazivTima: String!
+      ) {
+        AddIgracTim(
+          from: { brojTelefona: $brojTelefonaIgraca }
+          to: { naziv: $nazivTima }
+        ) {
+          from {
+            ime
+            prezime
             brojTelefona
+          }
+          to {
+            naziv
+          }
         }
-        }`;
-        this.query= this.apollo.watchQuery({
-            query:GET_IGRAC_BY_POSITION,
-            variables:{
-                pozicija:pozicija,
-                brojTelefonaIgraca:brojTelefona||null
-            }
-        });
-        return this.query.valueChanges;
-    }
-    getIgracByNumber(brojDresa:String,brojTelefona?:String)
-    {
-        let GET_IGRAC_BY_NUMBER=gql`
-        query GET_IGRAC_BY_NUMBER($brojDresa:String!,$brojTelefonaIgraca:String){
-        Igrac(filter:{
-            brojDresa:$brojDresa,brojTelefona_not:$brojTelefonaIgraca})
-            {
-            ime,
-            prezime,
-            brojDresa,
+      }
+    `;
+    return this.apollo.mutate({
+      mutation: ADD_IGRAC_TO_TIM,
+      variables: {
+        brojTelefonaIgraca: brojTelefonaIgraca,
+        nazivTima: nazivTima
+      }
+    });
+  }
+  addIgracGol(
+    brojTelefonaIgraca: String,
+    datumUtakmice: String,
+    vremeUtakmice: String,
+    vremeGola: String
+  ) {
+    let ADD_IGRAC_GOL_UTAKMICA = gql`
+      mutation ADD_IGRAC_GOL_UTAKMICA(
+        $brojTelefonaIgraca: String!
+        $datumUtakmice: String!
+        $vremeUtakmice: String!
+        $vremeGola: String!
+      ) {
+        AddIgracGolovi(
+          from: { brojTelefona: $brojTelefonaIgraca }
+          to: { datum: $datumUtakmice, vreme: $vremeUtakmice }
+          data: { vreme: $vremeGola }
+        ) {
+          from {
+            ime
+            prezime
             brojTelefona
-            }
-        }`;
-        this.query= this.apollo.watchQuery({
-            query:GET_IGRAC_BY_NUMBER,
-            variables:{
-                brojDresa:brojDresa,
-                brojTelefonaIgraca:brojTelefona||null
-            }
-        });
-        return this.query.valueChanges;
-    }
-    getIgrac(brojTelefonaIgraca:String)
-    {
-        let GET_IGRAC=gql`
-        query GET_IGRAC($brojTelefonaIgraca:String!){
-            Igrac(filter:{brojTelefona:$brojTelefonaIgraca})
-            {
-                ime,
-                prezime,
-                brojDresa,
-                brojTelefona,
-                opis,
-                pozicija,
-                tim{
-                  Tim{
-                    naziv
-                  }
-                },
-                utakmice{
-                  Utakmica{
-                    naziv
-                  }
-                },
-              golovi{
-                vreme
-              }
-            }
-            }`;
-        this.query= this.apollo.watchQuery({
-            query:GET_IGRAC,
-            variables:{
-                brojTelefonaIgraca:brojTelefonaIgraca
-            }
-        });
-        return this.query.valueChanges;
-    }
-    getTopStrelci()
-    {
-        let TOP_STRELCI=gql`
-        query TOP_STRELCI{
-        Igrac(filter:{golovi_not:null}){
-            ime,
-            brojTelefona,
-            golovi{
-                vreme
-            }
+          }
+          to {
+            naziv
+            datum
+            vreme
+          }
         }
-        }`;
-        this.query= this.apollo.watchQuery({
-            query:TOP_STRELCI
-        });
-        return this.query.valueChanges;
-    }
-    getTopIskustvo()
-    {
-        let TOP_ISKUSTVO=gql`
-        query TOP_ISKUSTVO{
-        Igrac(filter:{utakmice_not:null}){
-            ime,
-            utakmice
-            {
-                Utakmica
-                {
-                    naziv
-                }
-            }
+      }
+    `;
+    return this.apollo.mutate({
+      mutation: ADD_IGRAC_GOL_UTAKMICA,
+      variables: {
+        brojTelefonaIgraca: brojTelefonaIgraca,
+        datumUtakmice: datumUtakmice,
+        vremeUtakmice: vremeUtakmice,
+        vremeGola: vremeGola
+      }
+    });
+  }
+  addIgracToUtakmica(
+    brojTelefonaIgraca: String,
+    datumUtakmice: String,
+    vremeUtakmice: String
+  ) {
+    let ADD_IGRAC_U_UTAKMICU = gql`
+      mutation ADD_IGRAC_U_UTAKMICU(
+        $brojTelefonaIgraca: String!
+        $datumUtakmice: String!
+        $vremeUtakmice: String!
+      ) {
+        AddIgracUtakmice(
+          from: { brojTelefona: $brojTelefonaIgraca }
+          to: { datum: $datumUtakmice, vreme: $vremeUtakmice }
+        ) {
+          from {
+            ime
+            prezime
+            brojTelefona
+          }
+          to {
+            naziv
+            datum
+            vreme
+          }
         }
-        }`;
-        this.query= this.apollo.watchQuery({
-            query:TOP_ISKUSTVO
-        });
-        return this.query.valueChanges;
-    }
-
+      }
+    `;
+    return this.apollo.mutate({
+      mutation: ADD_IGRAC_U_UTAKMICU,
+      variables: {
+        brojTelefonaIgraca: brojTelefonaIgraca,
+        datumUtakmice: datumUtakmice,
+        vremeUtakmice: vremeUtakmice
+      }
+    });
+  }
+  getIgracStartsWith(pocetakImena: String) {
+    let GET_IGRAC_STARTS_WITH = gql`
+      query GET_IGRAC_STARTS_WITH($pocetakImena: String!) {
+        Igrac(filter: { ime_starts_with: $pocetakImena }) {
+          ime
+          prezime
+          brojDresa
+          brojTelefona
+          tim {
+            Tim {
+              naziv
+            }
+          }
+        }
+      }
+    `;
+    this.query = this.apollo.watchQuery({
+      query: GET_IGRAC_STARTS_WITH,
+      variables: {
+        pocetakImena: pocetakImena
+      }
+    });
+    return this.query.valueChanges;
+  }
+  getIgracByPosition(pozicija: String, brojTelefona?: String) {
+    let GET_IGRAC_BY_POSITION = gql`
+      query GET_IGRAC_BY_POSITION(
+        $pozicija: String!
+        $brojTelefonaIgraca: String
+      ) {
+        Igrac(
+          filter: { pozicija: $pozicija, brojTelefona_not: $brojTelefonaIgraca }
+        ) {
+          ime
+          prezime
+          brojDresa
+          brojTelefona
+        }
+      }
+    `;
+    this.query = this.apollo.watchQuery({
+      query: GET_IGRAC_BY_POSITION,
+      variables: {
+        pozicija: pozicija,
+        brojTelefonaIgraca: brojTelefona || null
+      }
+    });
+    return this.query.valueChanges;
+  }
+  getIgracByNumber(brojDresa: String, brojTelefona?: String) {
+    let GET_IGRAC_BY_NUMBER = gql`
+      query GET_IGRAC_BY_NUMBER(
+        $brojDresa: String!
+        $brojTelefonaIgraca: String
+      ) {
+        Igrac(
+          filter: {
+            brojDresa: $brojDresa
+            brojTelefona_not: $brojTelefonaIgraca
+          }
+        ) {
+          ime
+          prezime
+          brojDresa
+          brojTelefona
+        }
+      }
+    `;
+    this.query = this.apollo.watchQuery({
+      query: GET_IGRAC_BY_NUMBER,
+      variables: {
+        brojDresa: brojDresa,
+        brojTelefonaIgraca: brojTelefona || null
+      }
+    });
+    return this.query.valueChanges;
+  }
+  getIgrac(brojTelefonaIgraca: String) {
+    let GET_IGRAC = gql`
+      query GET_IGRAC($brojTelefonaIgraca: String!) {
+        Igrac(filter: { brojTelefona: $brojTelefonaIgraca }) {
+          ime
+          prezime
+          brojDresa
+          brojTelefona
+          opis
+          pozi
+          cija
+          tim {
+            Tim {
+              naziv
+            }
+          }
+          utakmice {
+            Utakmica {
+              naziv
+            }
+          }
+          golovi {
+            vreme
+          }
+        }
+      }
+    `;
+    this.query = this.apollo.watchQuery({
+      query: GET_IGRAC,
+      variables: {
+        brojTelefonaIgraca: brojTelefonaIgraca
+      }
+    });
+    return this.query.valueChanges;
+  }
+  getTopStrelci() {
+    let TOP_STRELCI = gql`
+      query TOP_STRELCI {
+        Igrac(filter: { golovi_not: null }) {
+          ime
+          brojTelefona
+          golovi {
+            vreme
+          }
+        }
+      }
+    `;
+    this.query = this.apollo.watchQuery({
+      query: TOP_STRELCI
+    });
+    return this.query.valueChanges;
+  }
+  getTopIskustvo() {
+    let TOP_ISKUSTVO = gql`
+      query TOP_ISKUSTVO {
+        Igrac(filter: { utakmice_not: null }) {
+          ime
+          utakmice {
+            Utakmica {
+              naziv
+            }
+          }
+        }
+      }
+    `;
+    this.query = this.apollo.watchQuery({
+      query: TOP_ISKUSTVO
+    });
+    return this.query.valueChanges;
+  }
 }
